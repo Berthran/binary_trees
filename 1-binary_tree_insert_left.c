17b6f0258566ce2143060e34d25508feb4f185e0
Daniel Berthran <berthlearn@gmail.com>
2024-06-05 11:22:40 +0100

    Debug IIIb - return changes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -72,7 +72,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		previousLeftChildNode->parent = leftChildNode;
 		parent->left = leftChildNode;
 		leftChildNode->left = previousLeftChildNode;
-		previousLeftChildNode->left = NULL;
 	}
 	return (leftChildNode);
 
