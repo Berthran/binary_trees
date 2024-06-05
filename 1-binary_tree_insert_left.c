85b262ed7ea2cc010ffcdaf5896ca6cebe17228f
Daniel Berthran <berthlearn@gmail.com>
2024-06-05 11:01:50 +0100

    Debug I - remove check parentDoesNotExists

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -52,8 +52,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	leftChildNode =  (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	if (parentDoesNotExist(parent))
-		return (NULL);
+	/*if (parentDoesNotExist(parent))
+		return (NULL); */
 	if (nodeCreationFailed(leftChildNode))
 		return (NULL);
 
