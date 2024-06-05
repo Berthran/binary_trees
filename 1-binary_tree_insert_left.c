5581584c1b4aaa73844896d283d84f4a8d3b27a1
Daniel Berthran <berthlearn@gmail.com>
2024-06-05 11:03:21 +0100

    Debug Ib - betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -52,8 +52,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	leftChildNode =  (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	/*if (parentDoesNotExist(parent))
-		return (NULL); */
 	if (nodeCreationFailed(leftChildNode))
 		return (NULL);
 
