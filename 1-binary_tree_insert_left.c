ef46be6ae832b354a54ee8b676ff78c386609f80
Daniel Berthran <berthlearn@gmail.com>
2024-06-05 11:21:57 +0100

    Debug III - force error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -72,6 +72,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		previousLeftChildNode->parent = leftChildNode;
 		parent->left = leftChildNode;
 		leftChildNode->left = previousLeftChildNode;
+		previousLeftChildNode->left = NULL;
 	}
 	return (leftChildNode);
 
