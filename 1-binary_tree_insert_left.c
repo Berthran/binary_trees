3d97bb8370ca1260f92b0eb4cb0a14903f776126
Daniel Berthran <berthlearn@gmail.com>
2024-06-07 02:04:35 +0100

    remove extra functions

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,42 +2,6 @@
 
 
 /**
- * parentDoesNotExist - checks if a parent node is NULL or not
- * @parent: the pointer to the parent node
- *
- * Return: 1 if true and 0 if false
- */
-
-int parentDoesNotExist(binary_tree_t *parent)
-{
-	int FALSE = 0;
-	int TRUE = 1;
-
-	if (parent == NULL)
-		return (TRUE);
-	return (FALSE);
-}
-
-
-/**
- * nodeCreationFailed - checks if a node is NULL or not
- * @node: the pointer to the node
- *
- * Return: 1 if true and 0 if false
- */
-
-int nodeCreationFailed(binary_tree_t *node)
-{
-	int FALSE = 0;
-	int TRUE = 1;
-
-	if (node == NULL)
-		return (TRUE);
-	return (FALSE);
-}
-
-
-/**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: the pointer to the node to insert the left-child in
  * @value: the value to store in the new node
@@ -54,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	if (nodeCreationFailed(leftChildNode))
+	if (leftChildNode == NULL)
 		return (NULL);
 
 	leftChildNode->parent = parent;
