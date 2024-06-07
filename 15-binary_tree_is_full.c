301cfb9d19aadae834da6232063790fe7b41b4f9
Daniel Berthran <berthlearn@gmail.com>
2024-06-07 03:05:41 +0100

    create a program to check if a binary tree is full --> attempt II (logical AND method)

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,29 +11,6 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t sizeOfTree;
-
-	if (tree == NULL)
-		return (0);
-
-	sizeOfTree = binary_tree_size(tree);
-
-	if (sizeOfTree % 2 == 0)
-		return (0);
-	return (1);
-}
-
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * The size is the number of nodes in the binary tree
- * @tree: a pointer to the root node of the tree to measure the size
- *
- * Return: the size of 0 if tree is NULL
- */
-
-size_t binary_tree_size(const binary_tree_t *tree)
-{
 	binary_tree_t *leftChild = NULL;
 	binary_tree_t *rightChild = NULL;
 
@@ -43,5 +20,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	leftChild = tree->left;
 	rightChild = tree->right;
 
-	return (1 + binary_tree_size(leftChild) + binary_tree_size(rightChild));
+	if (leftChild == NULL && rightChild == NULL)
+		return (1);
+	return (binary_tree_is_full(leftChild) && binary_tree_is_full(rightChild));
 }
