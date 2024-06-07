7bf6e4a22cbf191c715109b93f0c66e17d6d2159
Daniel Berthran <berthlearn@gmail.com>
2024-06-07 23:34:10 +0100

    create program to find the lowest common ancestor of two nodes in a binary tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 
+
+binary_tree_t *get_sibling(const binary_tree_t *node);
+
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: a pointer to the first node
@@ -9,12 +12,58 @@
  * Return NULL is no common ancestor is found.
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *parentOfFirst = NULL;
+	binary_tree_t *parentOfSecond = NULL;
+	binary_tree_t *siblingOfFirst = NULL;
+	binary_tree_t *closestAncestor = NULL;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	parentOfFirst = first->parent;
+	parentOfSecond = second->parent;
+
+	if (parentOfFirst == NULL && parentOfSecond != first)
+		return (NULL);
+	if (parentOfSecond == NULL && parentOfFirst != second)
+		return (NULL);
+
+	/* first and second are children of the same parent */
+	siblingOfFirst = get_sibling(first);
+	if (second == siblingOfFirst)
+		return (parentOfFirst);
+
+	/* first is a child of second */
+	if (first == parentOfSecond)
+		return (parentOfSecond);
+	/* second is a child of first */
+	if (second == parentOfFirst)
+		return (parentOfFirst);
+
+	/* first and second are children of different parent */
+	closestAncestor = binary_trees_ancestor(parentOfFirst, second);
+	if (closestAncestor == NULL)
+		closestAncestor = binary_trees_ancestor(parentOfSecond, first);
+	return (closestAncestor);
+
+}
+
+/**
+ * get_sibling - finds the sibling of a node.
+ * @node: a pointer to the node to find the sibling
+ *
+ * Return: pointer to the sibling or NULL if node is NULL,
+ * parent is NULL or node has no sibling
+ */
+
+binary_tree_t *get_sibling(const binary_tree_t *node)
 {
 	binary_tree_t *parent = NULL;
-	binary_tree_t *grandParent = NULL;
-	binary_tree_t *rightSon = NULL;
-	binary_tree_t *leftSon = NULL;
+	binary_tree_t *leftSibling = NULL;
+	binary_tree_t *rightSibling = NULL;
 
 	if (node == NULL)
 		return (NULL);
@@ -23,19 +72,14 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	if (parent == NULL)
 		return (NULL);
 
-	grandParent = parent->parent;
-	if (grandParent == NULL)
-		return (NULL);
+	leftSibling = parent->left;
+	rightSibling = parent->right;
 
-	leftSon = grandParent->left;
-	rightSon = grandParent->right;
-
-	if (parent == leftSon)
-		if (rightSon != NULL)
-			return (rightSon);
-	if (parent == rightSon)
-		if (leftSon != NULL)
-			return (leftSon);
+	if (node == leftSibling)
+		if (rightSibling != NULL)
+			return (rightSibling);
+	if (node == rightSibling)
+		if (leftSibling != NULL)
+			return (leftSibling);
 	return (NULL);
-
 }
